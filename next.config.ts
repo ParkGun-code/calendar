462a80ec523b700727a52595c989c4467014bda0
ParkGun-code <gun802000@gmail.com>
2026-09-15 19:02:21 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // 빌드 시 TypeScript 타입 에러가 있어도 무시하고 배포 진행
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // 빌드 시 ESLint 문법 에러가 있어도 무시하고 배포 진행
-    ignoreDuringBuilds: true,
-  },
+  // eslint 블록을 완전히 제거합니다.
 };
 
 export default nextConfig;
