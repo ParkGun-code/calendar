e82c6f39972b91f76da8c9ffb246f804a6c49ab3
ParkGun-code <gun802000@gmail.com>
2026-09-15 19:10:36 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // 빌드 중 타입 에러가 발생해도 빌드를 강제 통과시킵니다.
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
